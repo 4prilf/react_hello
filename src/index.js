a339192af05e156916ce3465396b35559a8755c1
Passiondown <a9979a458@gmail.com>
2024-03-27 11:18:28 +0800

    根只有一个，从根开始渲染

--- src/index.js
+++ src/index.js
@@ -18,11 +18,11 @@ rot.render(
     <App />
   </React.StrictMode>
 );
-
+*/
 setTimeout(()=>{
   root.unmount();
 },2000)
-*/
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
