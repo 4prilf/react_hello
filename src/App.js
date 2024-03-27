0a8b8fabfe4f468e0dd88ef92db9e9eead323b52
Passiondown <a9979a458@gmail.com>
2024-03-27 12:17:08 +0800

    setstate的运行逻辑

--- src/App.js
+++ src/App.js
@@ -1,46 +1,41 @@
 import React from 'react';
 import './App.css';
-
-//{}:调用JavaScript
 class App extends React.Component{
-  f1(){
-    console.log(this) 
+  state = {
+    a:0
   }
-  f2(a,b){//推荐使用
-    console.log(this,a,b)     
-    return function(){
-      console.log("return by f2",a,b)//并不显示
-    }
-  }
-  f3(a,b){
-    
-    console.log(this,a,b)//并不显示
-    a=a+1
-    return function(){
-      console.log("return by f3",a,b)
-    }
- 
+  b=123
+  addA(){
+  
+    this.setState({
+      a:888
+    })
+    console.log(this.state.a)//此时控制台打印的任然是0
+
+    //出发事件完成后才完全更新
+  
+    this.state.a=999
+    /*
+    this.setState({
+      //什么都不干进行更新
+    })
+    */
+    this.state.a=1000
+    console.log(this.state.a)//此时控制台打印的是1000
+    //页面显示居然是1000
+
+    return this.state.a
+//打印的state.a是addA中本身state.a的数值，在完成所有函数调用后才更新state.a
   }
   render(){
-    //去执行f1返回1的值
-    //去执行f3但是，返回执行f3的值
-    //f3首次渲染 67 点击渲染 return by function
-    //index加载app组件首次渲染执行6，7
-
-    return<div className="App">
-      <div onClick={this.f1}> 
-      undefine  123
-      </div>
-      <div onClick={this.f2.bind(this,1,2)}>
-        123    App
-      </div>
-      <div onClick={this.f3(6,7)}>
-        123    App
-      </div>
+    return <div className='App'>
+      {this.state.a}
+      <button onClick={this.addA.bind(this)}>change</button>
+      {this.b}
     </div>
   }
 }
-
-
+//结论：setdate是在完成所有函数回调后以当前的函数栈内对应数的值进行更新，setdata在哪都是完成函数回调后才执行，因此在书写时setdata要放在最后
+//事实逻辑就是如此
 
 export default App;
