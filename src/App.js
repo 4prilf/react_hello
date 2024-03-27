d416fd7232ce05f80828a5c3d2371820f9137ab8
Passiondown <a9979a458@gmail.com>
2024-03-27 11:48:27 +0800

    react component and jsx

--- src/App.js
+++ src/App.js
@@ -2,14 +2,40 @@ import React from 'react';
 import './App.css';
 
   
-class App extends React.Component{
-
-  render(){
-    return <div className='App'>
-      react入门
-    </div>
+function App() {
+  /*
+    function FnHello(){
+      return <div>hello</div>
+    }
+  */
+   //首字母大写
+    function FnHello(){
+      return React.createElement('div',[],'hello')
+    }
+  
+    class ClassHello extends React.Component{
+      constructor(props){
+        super(props)
+      }
+      render(){
+        return <div>hello class</div>
+      }
+    }
+    let com1 = <FnHello></FnHello>
+    let obj ={
+      a:1
+    }
+    let arr = [com1,2,3]
+    return (
+      <div className="App">
+      {false?<FnHello></FnHello>:obj.a}
+      {true?<FnHello></FnHello>:obj.a}
+      <FnHello></FnHello>
+      {arr}
+      <ClassHello></ClassHello>
+      </div>
+    );
   }
-}
 
 
 export default App;
