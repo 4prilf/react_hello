866bcb4d98380134f8418d18529d743557bf2de8
Passiondown <a9979a458@gmail.com>
2024-03-27 12:19:00 +0800

    setstate的浅合并

--- src/App.js
+++ src/App.js
@@ -1,41 +1,60 @@
 import React from 'react';
 import './App.css';
+//setdata的浅合并
 class App extends React.Component{
-  state = {
-    a:0
+  state ={//数据初始化，在index中引入，首次加载
+    a:0,
+    b:1,
+    c:{
+      c1:123,
+      c2:321
+    }
   }
-  b=123
-  addA(){
-  
+  addA = () => {//后续设置
     this.setState({
-      a:888
+      a:this.state.a+1,
+      b:2,
+      c:{
+        c1:9
+      }
     })
-    console.log(this.state.a)//此时控制台打印的任然是0
-
-    //出发事件完成后才完全更新
-  
-    this.state.a=999
-    /*
-    this.setState({
-      //什么都不干进行更新
-    })
-    */
-    this.state.a=1000
-    console.log(this.state.a)//此时控制台打印的是1000
-    //页面显示居然是1000
-
-    return this.state.a
-//打印的state.a是addA中本身state.a的数值，在完成所有函数调用后才更新state.a
+    console.log(this.state)//首次c1:123因为setdata实际最后执行
+    //第二次执行后c2直接没了，因为数组的软合并
   }
+  initC = () => {//后续设置
+    this.setState({
+      a:this.state.a+1,
+      b:2,
+      c:{
+        ...this.state.c,//声明一遍数组元素，防止软合并丢失
+        c1:9
+      }
+    })
+    console.log("initC",this.state)
+  }
+  initState = ()=>{
+    this.setState({
+      a:this.state.a+1,
+      b:2,
+      c:{
+        ...this.state.c,//声明一遍数组元素，防止软合并丢失
+        c1:9
+      }
+    },()=>{
+      console.log("initstate的setstate的第二个参数",this.state)//a=1
+    })
+    console.log("initstate的setstate下面",this.state)//a=0
+    //事实证明的确setstate就是在最后执行
+  }
+  //不要在render中调用setstate render->setstate->重新加载render
   render(){
     return <div className='App'>
       {this.state.a}
-      <button onClick={this.addA.bind(this)}>change</button>
-      {this.b}
+      <button onClick={this.addA}>a加1</button>
+      <button onClick={this.initC}>c2初始化</button>
+      <button onClick={this.initState}>setstate第二个参数获取state</button>
     </div>
   }
 }
-//结论：setdate是在完成所有函数回调后以当前的函数栈内对应数的值进行更新，setdata在哪都是完成函数回调后才执行，因此在书写时setdata要放在最后
-//事实逻辑就是如此
 
 export default App;
