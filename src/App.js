5357af993bf3eca43f16cf16945385d85a14b05d
Passiondown <a9979a458@gmail.com>
2024-03-27 11:52:15 +0800

    react事件绑定

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-  
 class App extends React.Component{
-
   render(){
-    return <div className='App'>
-      react入门
+    return<div className="App">
+      <div onClick={()=>{
+        console.log(1)
+      }}>
+        123
+      </div>
     </div>
   }
 }
